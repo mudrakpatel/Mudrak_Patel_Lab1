88ddaf6197691ce0172daa3f1810d6ec2f7d4168
mudrakpatel <mudrakpatel96@gmail.com>
2017-01-21 04:57:05 -0500

    Ex2 25% done; Facing Regular expression problem

---
+++ Mudrak_Patel_Lab1_Ex2/Ex2.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Mudrak_Patel_Lab1_Ex2
+{
+    partial class Ex2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupInformationGroupBox = new System.Windows.Forms.GroupBox();
+            this.phoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.provinceTextBox = new System.Windows.Forms.TextBox();
+            this.postalCodeTextBox = new System.Windows.Forms.TextBox();
+            this.cityTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.provinceLabel = new System.Windows.Forms.Label();
+            this.postalCodeLabel = new System.Windows.Forms.Label();
+            this.cityLabel = new System.Windows.Forms.Label();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.coursesGroupBox = new System.Windows.Forms.GroupBox();
+            this.computerProgrammerAnalystListBox = new System.Windows.Forms.ListBox();
+            this.computerProgrammerListBox = new System.Windows.Forms.ListBox();
+            this.computerProgrammerAnalystComboBox = new System.Windows.Forms.ComboBox();
+            this.computerProgrammerComboBox = new System.Windows.Forms.ComboBox();
+            this.radioButtonsPanel = new System.Windows.Forms.Panel();
+            this.computerProgrammerRadioButton = new System.Windows.Forms.RadioButton();
+            this.computerProgrammerAnalystRadioButton = new System.Windows.Forms.RadioButton();
+            this.displayButton = new System.Windows.Forms.Button();
+            this.groupInformationGroupBox.SuspendLayout();
+            this.coursesGroupBox.SuspendLayout();
+            this.radioButtonsPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupInformationGroupBox
+            // 
+            this.groupInformationGroupBox.Controls.Add(this.phoneNumberTextBox);
+            this.groupInformationGroupBox.Controls.Add(this.provinceTextBox);
+            this.groupInformationGroupBox.Controls.Add(this.postalCodeTextBox);
+            this.groupInformationGroupBox.Controls.Add(this.cityTextBox);
+            this.groupInformationGroupBox.Controls.Add(this.addressTextBox);
+            this.groupInformationGroupBox.Controls.Add(this.nameTextBox);
+            this.groupInformationGroupBox.Controls.Add(this.label1);
+            this.groupInformationGroupBox.Controls.Add(this.provinceLabel);
+            this.groupInformationGroupBox.Controls.Add(this.postalCodeLabel);
+            this.groupInformationGroupBox.Controls.Add(this.cityLabel);
+            this.groupInformationGroupBox.Controls.Add(this.addressLabel);
+            this.groupInformationGroupBox.Controls.Add(this.nameLabel);
+            this.groupInformationGroupBox.Location = new System.Drawing.Point(13, 13);
+            this.groupInformationGroupBox.Name = "groupInformationGroupBox";
+            this.groupInformationGroupBox.Size = new System.Drawing.Size(346, 200);
+            this.groupInformationGroupBox.TabIndex = 0;
+            this.groupInformationGroupBox.TabStop = false;
+            this.groupInformationGroupBox.Text = "Student information";
+            // 
+            // phoneNumberTextBox
+            // 
+            this.phoneNumberTextBox.Location = new System.Drawing.Point(115, 167);
+            this.phoneNumberTextBox.Name = "phoneNumberTextBox";
+            this.phoneNumberTextBox.Size = new System.Drawing.Size(219, 20);
+            this.phoneNumberTextBox.TabIndex = 11;
+            // 
+            // provinceTextBox
+            // 
+            this.provinceTextBox.Location = new System.Drawing.Point(115, 140);
+            this.provinceTextBox.Name = "provinceTextBox";
+            this.provinceTextBox.Size = new System.Drawing.Size(219, 20);
+            this.provinceTextBox.TabIndex = 10;
+            // 
+            // postalCodeTextBox
+            // 
+            this.postalCodeTextBox.Location = new System.Drawing.Point(115, 112);
+            this.postalCodeTextBox.Name = "postalCodeTextBox";
+            this.postalCodeTextBox.Size = new System.Drawing.Size(219, 20);
+            this.postalCodeTextBox.TabIndex = 9;
+            // 
+            // cityTextBox
+            // 
+            this.cityTextBox.Location = new System.Drawing.Point(115, 84);
+            this.cityTextBox.Name = "cityTextBox";
+            this.cityTextBox.Size = new System.Drawing.Size(219, 20);
+            this.cityTextBox.TabIndex = 8;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(115, 55);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(219, 20);
+            this.addressTextBox.TabIndex = 7;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(115, 28);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(219, 20);
+            this.nameTextBox.TabIndex = 6;
+            this.nameTextBox.Leave += new System.EventHandler(this.nameTextBox_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Phone number :";
+            // 
+            // provinceLabel
+            // 
+            this.provinceLabel.AutoSize = true;
+            this.provinceLabel.Location = new System.Drawing.Point(53, 140);
+            this.provinceLabel.Name = "provinceLabel";
+            this.provinceLabel.Size = new System.Drawing.Size(55, 13);
+            this.provinceLabel.TabIndex = 4;
+            this.provinceLabel.Text = "Province :";
+            // 
+            // postalCodeLabel
+            // 
+            this.postalCodeLabel.AutoSize = true;
+            this.postalCodeLabel.Location = new System.Drawing.Point(39, 112);
+            this.postalCodeLabel.Name = "postalCodeLabel";
+            this.postalCodeLabel.Size = new System.Drawing.Size(69, 13);
+            this.postalCodeLabel.TabIndex = 3;
+            this.postalCodeLabel.Text = "Postal code :";
+            // 
+            // cityLabel
+            // 
+            this.cityLabel.AutoSize = true;
+            this.cityLabel.Location = new System.Drawing.Point(78, 84);
+            this.cityLabel.Name = "cityLabel";
+            this.cityLabel.Size = new System.Drawing.Size(30, 13);
+            this.cityLabel.TabIndex = 2;
+            this.cityLabel.Text = "City :";
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Location = new System.Drawing.Point(57, 55);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(51, 13);
+            this.addressLabel.TabIndex = 1;
+            this.addressLabel.Text = "Address :";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(67, 28);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(41, 13);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Name :";
+            // 
+            // coursesGroupBox
+            // 
+            this.coursesGroupBox.Controls.Add(this.computerProgrammerAnalystListBox);
+            this.coursesGroupBox.Controls.Add(this.computerProgrammerListBox);
+            this.coursesGroupBox.Controls.Add(this.computerProgrammerAnalystComboBox);
+            this.coursesGroupBox.Controls.Add(this.computerProgrammerComboBox);
+            this.coursesGroupBox.Controls.Add(this.radioButtonsPanel);
+            this.coursesGroupBox.Location = new System.Drawing.Point(366, 13);
+            this.coursesGroupBox.Name = "coursesGroupBox";
+            this.coursesGroupBox.Size = new System.Drawing.Size(393, 200);
+            this.coursesGroupBox.TabIndex = 1;
+            this.coursesGroupBox.TabStop = false;
+            this.coursesGroupBox.Text = "Courses information";
+            // 
+            // computerProgrammerAnalystListBox
+            // 
+            this.computerProgrammerAnalystListBox.FormattingEnabled = true;
+            this.computerProgrammerAnalystListBox.Location = new System.Drawing.Point(11, 84);
+            this.computerProgrammerAnalystListBox.Name = "computerProgrammerAnalystListBox";
+            this.computerProgrammerAnalystListBox.Size = new System.Drawing.Size(323, 108);
+            this.computerProgrammerAnalystListBox.TabIndex = 4;
+            // 
+            // computerProgrammerListBox
+            // 
+            this.computerProgrammerListBox.FormattingEnabled = true;
+            this.computerProgrammerListBox.Location = new System.Drawing.Point(11, 84);
+            this.computerProgrammerListBox.Name = "computerProgrammerListBox";
+            this.computerProgrammerListBox.Size = new System.Drawing.Size(323, 108);
+            this.computerProgrammerListBox.TabIndex = 3;
+            // 
+            // computerProgrammerAnalystComboBox
+            // 
+            this.computerProgrammerAnalystComboBox.FormattingEnabled = true;
+            this.computerProgrammerAnalystComboBox.Items.AddRange(new object[] {
+            "Word Processing",
+            "Advance Programming",
+            "Computer Fundamentals and Software Engineering"});
+            this.computerProgrammerAnalystComboBox.Location = new System.Drawing.Point(13, 48);
+            this.computerProgrammerAnalystComboBox.Name = "computerProgrammerAnalystComboBox";
+            this.computerProgrammerAnalystComboBox.Size = new System.Drawing.Size(323, 21);
+            this.computerProgrammerAnalystComboBox.TabIndex = 2;
+            this.computerProgrammerAnalystComboBox.SelectedIndexChanged += new System.EventHandler(this.computerProgrammerAnalystComboBox_SelectedIndexChanged);
+            // 
+            // computerProgrammerComboBox
+            // 
+            this.computerProgrammerComboBox.FormattingEnabled = true;
+            this.computerProgrammerComboBox.Items.AddRange(new object[] {
+            "C Sharp",
+            "Java",
+            "Web Programming"});
+            this.computerProgrammerComboBox.Location = new System.Drawing.Point(11, 48);
+            this.computerProgrammerComboBox.Name = "computerProgrammerComboBox";
+            this.computerProgrammerComboBox.Size = new System.Drawing.Size(323, 21);
+            this.computerProgrammerComboBox.TabIndex = 1;
+            this.computerProgrammerComboBox.SelectedIndexChanged += new System.EventHandler(this.computerProgrammerComboBox_SelectedIndexChanged);
+            // 
+            // radioButtonsPanel
+            // 
+            this.radioButtonsPanel.Controls.Add(this.computerProgrammerRadioButton);
+            this.radioButtonsPanel.Controls.Add(this.computerProgrammerAnalystRadioButton);
+            this.radioButtonsPanel.Location = new System.Drawing.Point(7, 20);
+            this.radioButtonsPanel.Name = "radioButtonsPanel";
+            this.radioButtonsPanel.Size = new System.Drawing.Size(376, 21);
+            this.radioButtonsPanel.TabIndex = 0;
+            // 
+            // computerProgrammerRadioButton
+            // 
+            this.computerProgrammerRadioButton.AutoSize = true;
+            this.computerProgrammerRadioButton.Location = new System.Drawing.Point(243, 1);
+            this.computerProgrammerRadioButton.Name = "computerProgrammerRadioButton";
+            this.computerProgrammerRadioButton.Size = new System.Drawing.Size(128, 17);
+            this.computerProgrammerRadioButton.TabIndex = 1;
+            this.computerProgrammerRadioButton.Text = "Computer programmer";
+            this.computerProgrammerRadioButton.UseVisualStyleBackColor = true;
+            this.computerProgrammerRadioButton.CheckedChanged += new System.EventHandler(this.computerProgrammerRadioButton_CheckedChanged);
+            // 
+            // computerProgrammerAnalystRadioButton
+            // 
+            this.computerProgrammerAnalystRadioButton.AutoSize = true;
+            this.computerProgrammerAnalystRadioButton.Checked = true;
+            this.computerProgrammerAnalystRadioButton.Location = new System.Drawing.Point(4, 1);
+            this.computerProgrammerAnalystRadioButton.Name = "computerProgrammerAnalystRadioButton";
+            this.computerProgrammerAnalystRadioButton.Size = new System.Drawing.Size(166, 17);
+            this.computerProgrammerAnalystRadioButton.TabIndex = 0;
+            this.computerProgrammerAnalystRadioButton.TabStop = true;
+            this.computerProgrammerAnalystRadioButton.Text = "Computer programmer/analyst";
+            this.computerProgrammerAnalystRadioButton.UseVisualStyleBackColor = true;
+            this.computerProgrammerAnalystRadioButton.CheckedChanged += new System.EventHandler(this.computerProgrammerAnalystRadioButton_CheckedChanged);
+            // 
+            // displayButton
+            // 
+            this.displayButton.Location = new System.Drawing.Point(13, 220);
+            this.displayButton.Name = "displayButton";
+            this.displayButton.Size = new System.Drawing.Size(75, 23);
+            this.displayButton.TabIndex = 2;
+            this.displayButton.Text = "Display";
+            this.displayButton.UseVisualStyleBackColor = true;
+            // 
+            // Ex2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 348);
+            this.Controls.Add(this.displayButton);
+            this.Controls.Add(this.coursesGroupBox);
+            this.Controls.Add(this.groupInformationGroupBox);
+            this.Name = "Ex2";
+            this.Text = "Student Registration";
+            this.groupInformationGroupBox.ResumeLayout(false);
+            this.groupInformationGroupBox.PerformLayout();
+            this.coursesGroupBox.ResumeLayout(false);
+            this.radioButtonsPanel.ResumeLayout(false);
+            this.radioButtonsPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupInformationGroupBox;
+        private System.Windows.Forms.TextBox phoneNumberTextBox;
+        private System.Windows.Forms.TextBox provinceTextBox;
+        private System.Windows.Forms.TextBox postalCodeTextBox;
+        private System.Windows.Forms.TextBox cityTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label provinceLabel;
+        private System.Windows.Forms.Label postalCodeLabel;
+        private System.Windows.Forms.Label cityLabel;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.GroupBox coursesGroupBox;
+        private System.Windows.Forms.ComboBox computerProgrammerAnalystComboBox;
+        private System.Windows.Forms.ComboBox computerProgrammerComboBox;
+        private System.Windows.Forms.Panel radioButtonsPanel;
+        private System.Windows.Forms.RadioButton computerProgrammerRadioButton;
+        private System.Windows.Forms.RadioButton computerProgrammerAnalystRadioButton;
+        private System.Windows.Forms.ListBox computerProgrammerListBox;
+        private System.Windows.Forms.ListBox computerProgrammerAnalystListBox;
+        private System.Windows.Forms.Button displayButton;
+    }
+}
+
---
+++ Mudrak_Patel_Lab1_Ex2/Ex2.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Mudrak_Patel_Lab1_Ex2
+{
+    public partial class Ex2 : Form
+    {
+        public Ex2()
+        {
+            InitializeComponent();
+        }
+
+        private void computerProgrammerAnalystRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            switch (computerProgrammerAnalystRadioButton.Checked)
+            {
+                case true: computerProgrammerAnalystComboBox.Show();
+                           computerProgrammerAnalystListBox.Show();
+                           computerProgrammerComboBox.Hide();
+                           computerProgrammerListBox.Hide();
+                    break;
+            }
+        }
+
+        private void computerProgrammerRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            switch (computerProgrammerRadioButton.Checked)
+            {
+                case true:
+                    computerProgrammerComboBox.Show();
+                    computerProgrammerListBox.Show();
+                    computerProgrammerAnalystComboBox.Hide();
+                    computerProgrammerAnalystListBox.Hide();
+                    break;
+            }
+        }
+
+        private void computerProgrammerComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (computerProgrammerListBox.Items.Contains(computerProgrammerComboBox.SelectedItem))
+                {
+                    MessageBox.Show("One course can be added only one time.", "Selection error!");
+                }
+                else
+                {
+                    computerProgrammerListBox.Items.Add(computerProgrammerComboBox.SelectedItem);
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message,"Exception occured!");
+            }
+        }
+
+        private void computerProgrammerAnalystComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (computerProgrammerAnalystListBox.Items.Contains(computerProgrammerAnalystComboBox.SelectedItem))
+                {
+                    MessageBox.Show("One course can be added only one time.", "Selection error!");
+                }
+                else
+                {
+                    computerProgrammerAnalystListBox.Items.Add(computerProgrammerAnalystComboBox.SelectedItem);
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Exception occured!");
+            }
+        }
+
+        private void nameTextBox_Leave(object sender, EventArgs e)
+        {
+            validateTextBox(nameTextBox, new Regex(@"^((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}$"), "Please enter a valid phone number with country code.\nDonot leave this textbox empty or null.");
+        }
+
+        private void validateTextBox(TextBox textBoxToValidate, Regex validationExpression, string errorMessageToShow)
+        {
+            if (!(validationExpression.IsMatch(textBoxToValidate.Text)) || 
+                (String.IsNullOrWhiteSpace(textBoxToValidate.Text)))
+            {
+                MessageBox.Show(errorMessageToShow,"Validation error!");
+            }
+        }
+    }
+}
